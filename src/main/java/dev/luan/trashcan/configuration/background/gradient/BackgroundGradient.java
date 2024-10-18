5c939273416fadb2eaba3ae8b39966173e0c50bf
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-18 21:52:37 +0200

    Add Background configuration class
    Introduce the Background class with properties for image, color, blur, and gradient configurations. Includes methods to set default values, enabling flexible background configurations for the application.

---
+++ src/main/java/dev/luan/trashcan/configuration/background/gradient/BackgroundGradient.java
@@ -0,0 +1,21 @@
+package dev.luan.trashcan.configuration.background.gradient;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class BackgroundGradient {
+
+    List<String> colors;
+    GradientType gradientType;
+
+}
