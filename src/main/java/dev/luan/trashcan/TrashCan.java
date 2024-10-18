63a803aa7ef2b5fcba9c496ea59d1486b23a6962
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-18 21:40:53 +0200

    Add TrashCan main application class
    Introduced the `TrashCan` class, extending `Application` for JavaFX integration. Added a basic structure with `main` and `start` methods to serve as an entry point for the application.

---
+++ src/main/java/dev/luan/trashcan/TrashCan.java
@@ -0,0 +1,16 @@
+package dev.luan.trashcan;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TrashCan extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
